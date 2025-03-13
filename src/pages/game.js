559b03961bafcd2c612ad637b3b86f496e30a324
amzocean <amzocean@gmail.com>
2025-03-13 14:57:55 -0700

    Update game.js

--- src/pages/game.js
+++ src/pages/game.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 // Define summary text as a constant for easy changes.
-const summaryText = "Total 7 words - 4 LD | 3 EN";
+const summaryText = "Total 9 words - 8 LD | 1 EN";
 
 export default function Game() {
   const [game, setGame] = useState(null);
